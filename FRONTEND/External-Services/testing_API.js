f762eb85faf959ef2d74fda22a71e916d6b49588
LaggyMcLagg <64869611+LaggyMcLagg@users.noreply.github.com>
2023-07-06 13:47:33 +0100

    Multi use Post & Get
    - Limpeza de código obsoleto;
    - Doc Bloc para API.js & productClass.js;
    - Relocalização da função makeRequests para a camada External-Services;
    - Criação de funções adaptativas (POST & GET) para comunicação com a API no main.js;
    - Doc bloc para as funções criadas.

--- FRONTEND/External-Services/testing_API.js
+++
@@ -1,6 +0,0 @@
-export async function getProduct(searchParam){
-    const _Response = await fetch("http://127.0.0.1:3333/products/");
-    const data = await _Response.json();
-
-    return data;
-}
--- FRONTEND/User-Interface/JSMAIN/main.js
+++ FRONTEND/User-Interface/JSMAIN/main.js
@@ -1,77 +1,97 @@
-// import { getProduct } from "../../External-Services/testing_API.js"
-// import { Product } from "../../Data-Modeling/testing_models.js"
+/**
+* MAIN.JS DOC BLOC
+*
+* IMPORTS--------------------------------------------------------
+* @module makeRequests: importado de '../../External-Services/API.js', função para fazer requisições GET e POST para a API.
+* @module Product: importado de '../../Data-Modeling/productClass.js', class 'Product' que serve para 
+* a instaciação de objectos (produto) com base na informação retornada pela API. 
+* --------------------------------------------------------------- 
+*
+* POST COUPON----------------------------------------------------
+* @function postCoupon: função que envia um código de cupão para a API e regista a resposta.
+* @param {string} couponCode: o código do cupão a ser enviado.
+* @returns {Promise} uma promise que, quando resolvida, regista a resposta da API.
+* --------------------------------------------------------------- 
+*
+* GET PRODUCTS---------------------------------------------------
+* @function getProducts: função que solicita uma lista de produtos à API e retorna os produtos como instâncias da classe Product.
+* @returns {Promise} uma promise que, quando resolvida, retorna um array de instâncias da classe Product.
+* --------------------------------------------------------------- 
+*
+* POST CHECKOUT--------------------------------------------------
+* @function postCheckout: função que envia uma lista de produtos e um cupão para a API e regista a resposta.
+* @param {Array} products: a lista de produtos a ser enviada.
+* @param {string} coupon: o cupão a ser enviado.
+* @returns {Promise} uma promise que, quando resolvida, regista a resposta da API.
+* --------------------------------------------------------------- 
+*
+*/
 
-// getProduct().then(product => {
-//     const product_ = new Product(product)
-
-//     console.log(product_)
-    
-//     console.log(product_[1].id)
-//     console.log(product_[1].name)
-//     console.log(product_[1].description)
-// })
-
-
-// API CONNECTION FUNCIONAL
-import { api } from '../../External-Services/API.js';
+//API CONNECTION AND PRODUCT CLASS
+import { makeRequests } from '../../External-Services/API.js';
 import { Product } from "../../Data-Modeling/productClass.js"
 
 
-async function makeRequests(endpoint, method, data = null) {
-    let response;
-    if (method.toLowerCase() === 'post') {
-      response = await api.post(endpoint, data);
-    } else if (method.toLowerCase() === 'get') {
-      response = await api.get(endpoint);
-    } else {
-      throw new Error(`Unsupported method: ${method}`);
-    }
-    
-    return response;
+  //POST COUPON
+  function postCoupon(couponCode) {
+    const couponJson = { "couponCode": couponCode };
+  
+    return makeRequests('/check-coupon', 'post', couponJson)
+      .then(response => {
+        return response;
+      })
+      .catch(console.error);
+  }
+  
+  //GET PRODUCTS
+  function getProducts() {
+    return makeRequests('/products', 'get')
+      .then(response => {
+        return response.map(
+          item => new Product(item.id, item.name, item.description, item.image, item.price, item.quantity)
+        );
+      })
+      .catch(console.error);
+  }
+  
+  //POST CHECKOUT
+  function postCheckout(products, coupon) {
+    const checkoutPostJson = {
+      "products": products,
+      "coupon": coupon
+    };
+  
+    return makeRequests('/checkout', 'post', checkoutPostJson)
+      .then(response => {
+        return response;
+      })
+      .catch(console.error);
   }
 
+//******************************************
+// TEST CALLS FOR API
+async function testApiConnection() {
+  console.log('Teste post postCoupon...');
+  const couponResponse = await postCoupon("IVMUSVV");
+  console.log(couponResponse);
 
-// ******************************************
-// TESTS FOR API
+  console.log('Teste getProducts...');
+  const products = await getProducts();
+  products.forEach(product => product.output());
 
-//POST COUPON
-const couponJson = { "couponCode": "IVMUSVV" };
+  console.log('Teste postCheckout...');
+  const checkoutResponse = await postCheckout([
+    {
+      "id": 1,
+      "quantity": 2
+    },
+    {
+      "id": 2,
+      "quantity": 1
+    }
+  ], "");
+  console.log(checkoutResponse);
+}
 
-makeRequests('/check-coupon', 'post', couponJson)
-  .then(response => {
-    console.log(response);
-  })
-  .catch(console.error);
-
-//GET
-makeRequests('/products', 'get')
-  .then(response => {
-    const products = response.map(
-      item => new Product(item.id, item.name, item.description, item.image, item.price, item.quantity)
-    );
-    products.forEach(product => console.log(product.toObject()));
-  })
-  .catch(console.error);
-
-//POST CHECKOUT
-const checkoutPostJson = {
-    "products": [
-      {
-        "id": 1,
-        "quantity": 2
-      },
-      {
-        "id": 2,
-        "quantity": 1
-      }
-    ],
-    "coupon": ""
-  };
-  
-  makeRequests('/checkout', 'post', checkoutPostJson)
-    .then(response => {
-      console.log(response);
-    })
-    .catch(console.error);
-
+testApiConnection();
 //******************************************************
