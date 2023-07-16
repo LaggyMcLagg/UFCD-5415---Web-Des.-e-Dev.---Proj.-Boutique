42ae68ffe261d505b6228fabcf5184299dd340f6
UFCD5409 - PL01 - Miguel <miguel.teixeira.t0124399@edu.atec.pt>
2023-07-16 20:21:31 +0100

    bug fix, js update, html modification
    finished contactForms.js;
    added some lines in CssContact.css;
    slight changes to contact.html;
    bug fix on randomContacts.js;

--- FRONTEND/kirei/logic/contactForm.js
+++ FRONTEND/kirei/logic/contactForm.js
@@ -1,17 +1,40 @@
-const name = document.getElementById('Name');
+const nome = document.getElementById('Name');
 const email = document.getElementById('Email');
-const textBox = document.getElementById('TexBox');
-const form = document.querySelector('.contact-form')
-const errorElement = document.getElementById('error');
+const textBox = document.getElementById('TextBox');
+const form = document.querySelector('.contact-form');
+
+nome.addEventListener('input', () => {
+    nome.classList.remove('invalid');
+    nome.nextElementSibling.classList.add('hidden');
+});
+  
+email.addEventListener('input', () => {
+    email.classList.remove('invalid');
+    email.nextElementSibling.classList.add('hidden');
+});
+  
+textBox.addEventListener('input', () => {
+    textBox.classList.remove('invalid');
+    textBox.nextElementSibling.classList.add('hidden');
+});
 
 form.addEventListener('submit', (e) => {
-    let messages = []
-    if (name.value === '' || name.value == null) {
-        messages.push('Name is required')
+
+    if(!nome.value.match(/^[a-zA-ZÀ-ÿ]+(\s{1}[a-zA-ZÀ-ÿ]+)*$/)) {
+        e.preventDefault();
+        nome.classList.add('invalid');
+        nome.nextElementSibling.classList.remove('hidden');
     }
 
-    if(messages.length > 0) {
-        e.preventDefault()
-        error
+    if (!email.value.match(/^[a-zA-Z0-9]+([._-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-][a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$/)) {
+        e.preventDefault();
+        email.classList.add('invalid');
+        email.nextElementSibling.classList.remove('hidden');
     }
+
+    if (textBox.value == '' || textBox.value == null) {
+        e.preventDefault();
+        textBox.classList.add('invalid');
+        textBox.nextElementSibling.classList.remove('hidden');
+    }     
 });
